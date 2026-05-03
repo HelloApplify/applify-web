94aef9b5ed85d606f68f541c8666c746c5939d4c
HelloApplify <helloapplify@gmail.com>
2026-05-03 07:19:34 -0400

    Fix: Update Applify logo geometry to exactly match target design

--- components/ApplifyLogo.tsx
+++ components/ApplifyLogo.tsx
@@ -8,9 +8,8 @@ export function ApplifyLogo({ className = "w-6 h-6" }: { className?: string }) {
       fill="currentColor" 
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M 5 90 L 35 10 L 47 10 L 17 90 Z" />
-      <path d="M 25 90 L 55 10 L 67 10 L 97 90 L 85 90 L 61 26 L 37 90 Z" />
-      <path d="M 47.5 62 L 74.5 62 L 79 74 L 43 74 Z" />
+      <path d="M 32.4 19.2 L 46.48 19.2 L 15.68 80.8 L 1.6 80.8 Z" />
+      <path d="M 53.52 19.2 L 67.6 19.2 L 98.4 80.8 L 84.32 80.8 L 78.16 68.48 L 28.88 68.48 L 35.92 54.4 L 71.12 54.4 Z" />
     </svg>
   )
 }
